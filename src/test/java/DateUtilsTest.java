0a38f42aae91c9897da28c00d272c2ef6a22f2a1
luciano.fernandes <luciano.fernandes@ntendencia.com.br>
2020-04-09 14:55:56 -0300

    Criação dos teste unitarios

---
+++ src/test/java/DateUtilsTest.java
@@ -0,0 +1,24 @@
+import br.ce.wcaquino.taskbackend.utils.DateUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+public class DateUtilsTest {
+    @Test
+    public void deveRetornarTrueParaDatasFuturas(){
+        LocalDate date = LocalDate.of(2030, 01,01);
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+    }
+
+    @Test
+    public void deveRetornarFalseParaDatasPassadas(){
+        LocalDate date = LocalDate.of(2010, 01,01);
+        Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
+    }
+
+    @Test
+    public void deveRetornarTrueParaDatasAtual(){
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(LocalDate.now()));
+    }
+}
